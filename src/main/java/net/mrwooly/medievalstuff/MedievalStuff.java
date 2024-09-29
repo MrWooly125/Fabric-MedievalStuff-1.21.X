f8b5ea27822d8a3cbbd174f0e669d9bf3573b234
MrWooly125 <belov.20.04@mail.ru>
2024-09-29 16:41:56 +0300

    Initial commit

---
+++ src/main/java/net/mrwooly/medievalstuff/MedievalStuff.java
@@ -0,0 +1,16 @@
+package net.mrwooly.medievalstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MedievalStuff implements ModInitializer {
+	public static final String MOD_ID = "medievalstuff";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mrwooly/medievalstuff/MedievalStuffClient.java
@@ -0,0 +1,10 @@
+package net.mrwooly.medievalstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MedievalStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
